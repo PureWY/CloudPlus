80f9602251c29ad7ad0dd5fbdbac65148289c37b
WY <you@example.com>
2019-06-26 22:36:45 +0800

    优化主页面，未完成

--- App/Navigation/AppNavigation.js
+++ App/Navigation/AppNavigation.js
@@ -1,15 +1,53 @@
-import { createStackNavigator, createAppContainer } from 'react-navigation'
-import Login from '../Views/Login/Login'
-import Home from '../Views/Home/Home'
+import { createStackNavigator, createBottomTabNavigator, createAppContainer } from 'react-navigation';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+import Login from '../Views/Login/Login';
+import Home from '../Views/Home/Home';
+import User from '../Views/User/User';
+import Message from '../Views/Message/Message';
 
-import styles from './Styles/NavigationStyles'
+import styles from './Styles/NavigationStyles';
+
+const TabNav = createBottomTabNavigator({
+    Home: {
+      screen: Home
+    },
+    Message: {
+      screen: Message
+    },
+    User: {
+      screen: User
+    }
+}, {
+    swipeEnabled: true,
+    initialRouteName: 'Home',
+    defaultNavigationOptions: ({ navigation }) => ({
+        tabBarIcon: ({ focused, horizontal, tintColor }) => {
+            const { routeName } = navigation.state;
+            let FontAwesomeComponent = FontAwesome5;
+            let iconName;
+            if (routeName === 'Home') {
+                iconName = `home`;
+            } else if (routeName === 'Message') {
+                iconName = `comment-dots`;
+            }else if(routeName === 'User') {
+                iconName = `user`
+            }
+
+            return <FontAwesomeComponent name={iconName} size={25} color={tintColor} />;
+        },
+    }),
+    tabBarOptions: {
+        activeTintColor: 'blue',
+        inactiveTintColor: 'gray',
+    },
+})
 
 const PrimaryNav = createStackNavigator({
+    TabBar: {
+        screen: TabNav
+    },
     Login: {
         screen: Login
-    },
-    Home: {
-        screen: Home
     }
 },{
     headerMode: 'none',
---
+++ App/Views/Message/Message.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react'
+import { ScrollView, Text, Image, View, StatusBar } from 'react-native'
+import { Button, InputItem, List, WhiteSpace } from '@ant-design/react-native';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import { connect } from 'react-redux'
+import { loginRequest } from '../../Actions/LoginAction'
+
+// Styles
+import styles from './Styles/MessageStyle'
+
+class Message extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userName: '',
+      passWord: ''
+    };
+  }
+
+  componentDidMount(){
+    console.log(this.props)
+  }
+
+  render () {
+    return (
+      <View style={styles.mainContainer}>
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor="#5175F0"
+        />
+        
+        <Text>消息页面</Text>
+      </View>
+    )
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+      // loginRequest: (userName, passWord) => dispatch(loginRequest(userName, passWord))
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+      state: state.login
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Message)
