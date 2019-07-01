91fc2a59a5c75a9dcc97d9193acb78cee81a5506
WY <you@example.com>
2019-07-01 16:24:03 +0800

    再按一次退出应用

--- App/Navigation/ReduxNavigation.js
+++ App/Navigation/ReduxNavigation.js
@@ -11,44 +11,46 @@ const ReduxAppNavigator = createReduxContainer(AppNavigation)
 
 class ReduxNavigation extends React.Component {
     lastBackPressed = 0;
+  
+    componentDidMount () {
+      if (Platform.OS === 'ios') return
+      BackHandler.addEventListener('hardwareBackPress', () => {
+        const { dispatch, nav } = this.props
+        console.log(nav.routes);
+  
+        if (nav.routes.length === 2 && nav.routes[1].routes[0].routeName === 'Home') {
+          if (this.lastBackPressed && this.lastBackPressed + 2000 >= Date.now()) {
+            //最近2秒内按过back键，可以退出应用。
+            dispatch({ type: 'Navigation/BACK' })
+            BackHandler.exitApp();
+            return false;
+          }
+  
+          this.lastBackPressed = Date.now();
+          ToastAndroid.show('再按一次退出应用', ToastAndroid.SHORT);
+          return true;
+        }
+  
+        // change to whatever is your first screen, otherwise unpredictable results may occur
+        if (nav.routes.length === 1 && (nav.routes[0].routeName === 'Login')) {
+            console.log('当前是登录页，不返回')
+            return false
+        }
 
-    componentDidMount(){
-        if(Platform.OS === 'ios') return
-        BackHandler.addEventListener('hardwareBackPress', () => {
-            const { dispatch, nav} = this.props
-
-            if(nav.routes.length === 2 && nav.routes[1].routeName === 'Home'){
-                if(this.lastBackPressed && this.lastBackPressed + 2000 >= Date.now()){
-                    PCMAudio.close();
-                    //最近2秒按过back键，可退出应用
-                    dispatch({ type: 'Navigation/BACK' })
-                    BackHandler.exitApp()
-                    return false;
-                }
-
-                this.lastBackPressed = Date.now();
-                ToastAndroid.show('再按一次退出应用', ToastAndroid.SHORT);
-                return true
-            }
-
-            if(nav.routes.length === 1 && (nav.routes.routeName === 'Login')){
-                return false
-            }
-
-            dispatch({ type: 'Navigation/BACK'})
-            return true
-        })
+        dispatch({ type: 'Navigation/BACK' })
+        return true
+      })
     }
-
-    componentWillUnmount(){
-        if(Platform.OS === 'ios') return
-        BackHandler.removeEventListener('hardwareBackPress', undefined)
+  
+    componentWillUnmount () {
+      if (Platform.OS === 'ios') return
+      BackHandler.removeEventListener('hardwareBackPress', undefined)
     }
-
-    render(){
-        return <ReduxAppNavigator dispatch={this.props.dispatch} state={this.props.nav} />
+  
+    render () {
+      return <ReduxAppNavigator dispatch={this.props.dispatch} state={this.props.nav} />
     }
-}
+  }
 
 const mapStateToProps = state => ({
     nav: state.nav
