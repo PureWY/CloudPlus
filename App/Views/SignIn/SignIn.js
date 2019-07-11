09b7876c30b465702a94b60663656ff72b45f088
WY <you@example.com>
2019-07-11 13:52:03 +0800

    每日签到页面完成

--- App/Views/SignIn/SignIn.js
+++ App/Views/SignIn/SignIn.js
@@ -6,10 +6,37 @@ import { connect } from 'react-redux'
 
 // Styles
 import styles from './Styles/SignInStyle'
+import { Colors, Metrics, Fonts } from '../../Themes';
 
 export default class SignIn extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      signDay: ['2019-07-09','2019-07-10'],
+      chooseDay: {},
+      allSignDay: {},
+      canSign: true
+    }
+  }
+
+  addSignIn(day){
+    let chooseStatus = { selected: true, marked: false, selectedColor: Metrics.mainBackgroundColor }
+    let signDay = { selected: true, marked: true, selectedColor: 'blue' }
+
+    // 选择日期
+    let dayString = day.dateString
+
+    let chooseDay = { 
+      [dayString]: chooseStatus
+    }
+    this.setState({
+      chooseDay: chooseDay
+    })
+
+    // 点击签到
+
+    console.log(day)
   }
 
   render () {
@@ -18,16 +45,35 @@ export default class SignIn extends Component {
       <View style={styles.mainContainer}>
         <ScrollView>
           <View style={styles.signHeader}>
-          <CalendarList
-          // Callback which gets executed when visible months change in scroll view. Default = undefined
-          onVisibleMonthsChange={(months) => {console.log('now these months are visible', months);}}
-          // Max amount of months allowed to scroll to the past. Default = 50
-          pastScrollRange={50}
-          // Max amount of months allowed to scroll to the future. Default = 50
-          futureScrollRange={50}
-          // Enable or disable scrolling of calendar list
-          scrollEnabled={true}
-        />
+            <CalendarList
+            theme={{
+              backgroundColor: '#f3f3f3',
+              indicatorColor: 'blue',
+              textDayFontWeight: '600',
+              textMonthFontWeight: 'bold',
+              textDayHeaderFontWeight: '300',
+              textDayFontSize: Fonts.size.input,
+              textMonthFontSize: Fonts.size.input,
+              textDayHeaderFontSize: 16
+            }}
+            pastScrollRange={50}
+            futureScrollRange={50}
+            onDayPress={(day) => {this.addSignIn(day)}}
+            horizontal={true}
+            maxDate={(new Date())}
+            monthFormat={'yyyy年MM月'}
+            markedDates={this.state.allSignDay}
+          />
+          </View>
+          <View style={styles.c_signFooter}>
+            <View style={styles.c_signDays}>
+              <Text style={styles.b_signDay}>已签到10天，云加期待每一天与你相见</Text>
+            </View>
+            <View style={styles.c_signBtn}>
+              <View style={styles.b_signBtn}>
+                <Text style={this.state.canSign?styles.b_signBtnFont:styles.b_signBtnFontGrey}>点击签到</Text>
+              </View>
+            </View>
           </View>
         </ScrollView>
       </View>
--- App/Views/SignIn/Styles/SignInStyle.js
+++ App/Views/SignIn/Styles/SignInStyle.js
@@ -1,28 +1,55 @@
 import { StyleSheet } from 'react-native'
-import { Metrics, ApplicationStyles, Colors } from '../../../Themes/'
+import { Metrics, ApplicationStyles, Colors, Fonts } from '../../../Themes/'
 
 export default StyleSheet.create({
   ...ApplicationStyles.screen,
   signHeader: {
     width: Metrics.screenWidth,
-    height: Metrics.screenHeight/2
+    height: Metrics.screenHeight/2,
+    backgroundColor: '#f3f3f3'
   },
-  calendar: {
-    borderTopWidth: 1,
-    paddingTop: 5,
-    borderBottomWidth: 1,
-    borderColor: '#eee',
-    height: 350
+  c_signFooter: {
+    backgroundColor: '#f3f3f3',
+    alignItems: 'center'
   },
-  text: {
-    textAlign: 'center',
-    borderColor: '#bbb',
-    padding: 10,
-    backgroundColor: '#eee'
+  c_signDays: {
+    width: Metrics.screenWidth/1.1,
+    height: 60,
+    backgroundColor: Colors.snow,
+    borderRadius: 10,
+    marginTop: Metrics.baseMargin,
+    padding: Metrics.basePadding,
+    justifyContent: 'center'
   },
-  container: {
-    flex: 1,
-    backgroundColor: 'gray'
-  }
-
+  b_signDay: {
+    fontSize: Fonts.size.medium,
+    color: Colors.black
+  },
+  c_signBtn: {
+    width: Metrics.screenWidth/1.1,
+    height: 160,
+    backgroundColor: Colors.snow,
+    borderRadius: 10,
+    marginTop: Metrics.baseMargin,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  b_signBtn: {
+    width: 140,
+    height: 140,
+    borderRadius: 70,
+    backgroundColor: Metrics.mainBackgroundColor,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  b_signBtnFont: {
+    color: Colors.snow,
+    fontSize: Fonts.size.h4,
+    fontWeight: '600'
+  },
+  b_signBtnFontGrey: {
+    color: '#ccc',
+    fontSize: Fonts.size.h4,
+    fontWeight: '600'
+  },
 })
